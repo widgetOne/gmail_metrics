cf8bcdad1407c722a8aa824a2b23031a1dd0fb6a
Brian Coulter <bcoulter@rentjungle.com>
2018-01-30 19:35:34 -0500

    a copy of the gmail python starter
    a copy of the gmail quickstart file to use as the
    base of the metrics collection logic

---
+++ gmail_api_quickstart.py
@@ -0,0 +1,75 @@
+'''
+Copied wholesale on 2018-01-18 from https://developers.google.com/gmail/api/quickstart/python
+Only the credential_path in get_credentials() was changed to be local
+Their copyright info:
+    Except as otherwise noted, the content of this page is licensed under the Creative Commons
+    Attribution 3.0 License, and code samples are licensed under the Apache 2.0 License. 
+    For details, see our Site Policies. Java is a registered trademark of Oracle and/or its affiliates.
+'''
+from __future__ import print_function
+import httplib2
+import os
+
+from apiclient import discovery
+from oauth2client import client
+from oauth2client import tools
+from oauth2client.file import Storage
+
+try:
+    import argparse
+    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
+except ImportError:
+    flags = None
+
+# If modifying these scopes, delete your previously saved credentials
+# at ~/.credentials/gmail-python-quickstart.json
+SCOPES = ['https://www.googleapis.com/auth/gmail.readonly', 'https://www.googleapis.com/auth/gmail.send']
+CLIENT_SECRET_FILE = 'client_secret.json'
+APPLICATION_NAME = 'Gmail API Python Quickstart'
+
+
+def get_credentials():
+    """Gets valid user credentials from storage.
+
+    If nothing has been stored, or if the stored credentials are invalid,
+    the OAuth2 flow is completed to obtain the new credentials.
+
+    Returns:
+        Credentials, the obtained credential.
+    """
+    credential_path = 'gmail_metrics_auth.json'
+    store = Storage(credential_path)
+    credentials = store.get()
+    if not credentials or credentials.invalid:
+        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
+        flow.user_agent = APPLICATION_NAME
+        if flags:
+            credentials = tools.run_flow(flow, store, flags)
+        else: # Needed only for compatibility with Python 2.6
+            credentials = tools.run(flow, store)
+        print('Storing credentials to ' + credential_path)
+    return credentials
+
+def main():
+    """Shows basic usage of the Gmail API.
+
+    Creates a Gmail API service object and outputs a list of label names
+    of the user's Gmail account.
+    """
+    credentials = get_credentials()
+    http = credentials.authorize(httplib2.Http())
+    service = discovery.build('gmail', 'v1', http=http)
+
+    results = service.users().labels().list(userId='me').execute()
+    labels = results.get('labels', [])
+
+    if not labels:
+        print('No labels found.')
+    else:
+      print('Labels:')
+      for label in labels:
+        print(label['name'])
+
+
+if __name__ == '__main__':
+    main()
